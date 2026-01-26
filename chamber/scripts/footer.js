b4229dbe1ea18f5d61674a949bd71a5ce7771851
NonatoDEv <anonatoavalos@byupathway.edu>
2026-01-25 20:33:06 -0500

    refactor: update directory and index HTML structure, add new styles for directory, and remove unused CSS files

--- chamber/scripts/footer.js
+++ chamber/scripts/footer.js
@@ -20,7 +20,7 @@ const footerHTML = `
 
         <div class="footer-legal">
             <p>&copy; ${new Date().getFullYear()} | Powered by Abel J. Nonato | Chincha, Perú</p>
-            <p id="lastModified">Modified: ${document.lastModified}</p>
+            <p id="lastModified"> last Modified: ${document.lastModified}</p>
         </div>
     </div>
 `;
